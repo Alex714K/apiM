a026eb452cb1dd0f15350833a50b710f30e854da
Александр <klimentenko.ad@gmail.com>
2024-03-30 01:46:48 +0300

    Почти рабочий вариант форматирования столбца с nmId

--- convert_to_list.py
+++ convert_to_list.py
@@ -19,7 +19,7 @@ def beta_start(file: list) -> tuple[list, int]:
     return ans, len(ans)
 
 
-def convert_to_list(file: list | dict, name_of_sheet: str) -> tuple[list, int] | bool:
+def convert_to_list(file: list | dict, name_of_sheet: str) -> tuple[list, int, list] | bool:
     if name_of_sheet in ['orders_1mnth', 'orders_1week', 'orders_2days', 'orders_today', 'stocks', 'rk']:
         return orders(file=file)
     elif name_of_sheet == 'prices':
@@ -30,13 +30,14 @@ def convert_to_list(file: list | dict, name_of_sheet: str) -> tuple[list, int] |
         return download(file=file, name=name_of_sheet)
 
 
-def orders(file: list) -> tuple[list, int]:
+def orders(file: list) -> tuple[list, int, list]:
     """Конвертирует json-объект в список, который подходит для добавления данных из файла в Google Excel"""
     if type(file) is type(None):
         sys.exit('file = None')
     keys = list()
     for key in file[0].keys():
         keys.append(key)
+    needed_keys = check_keys(keys)
     ans = numpy.array([keys])
     for i, row in enumerate(file):
         values = list()
@@ -44,20 +45,22 @@ def orders(file: list) -> tuple[list, int]:
             values.append(value)
         values = numpy.array([values])
         ans = numpy.concatenate((ans, values), axis=0)
-    return ans.tolist(), ans.shape[0]
+    return ans.tolist(), ans.shape[0], needed_keys
 
 
-def prices(file: dict) -> tuple[list, int]:
+def prices(file: dict) -> tuple[list, int, list]:
     if type(file) is type(None):
         sys.exit('file = None')
     file = file["data"]["listGoods"]
     ans = list()
-    ans.append([])
+    keys = list()
     for key, value in file[0].items():
         if type(value) is list:
-            ans[0].extend([key for key, value in value[0].items()])
+            keys.extend([key for key, value in value[0].items()])
         else:
-            ans[0].append(key)
+            keys.append(key)
+    ans.append(keys)
+    needed_keys = check_keys(keys)
     # print(json.dumps(file, ensure_ascii=False, indent=4))
     for i, row in enumerate(file):
         ans.append([])
@@ -68,10 +71,10 @@ def prices(file: dict) -> tuple[list, int]:
                 ans[i+1].append(value)
     for i in range(len(ans)):
         ans[i] = list(map(lambda x: str(x), ans[int(i)]))
-    return ans, len(ans)
+    return ans, len(ans), needed_keys
 
 
-def tariffs(file: dict) -> tuple[list, int]:
+def tariffs(file: dict) -> tuple[list, int, list]:
     if type(file) is type(None):
         sys.exit('file = None')
     with open('date_of_tariffs.txt', 'w') as txt:
@@ -79,6 +82,7 @@ def tariffs(file: dict) -> tuple[list, int]:
     keys = list()
     for key in file['response']['data']['warehouseList'][0].keys():
         keys.append(key)
+    needed_keys = check_keys(keys)
     ans = numpy.array([keys])
     for i, row in enumerate(file['response']['data']['warehouseList']):
         values = list()
@@ -86,7 +90,7 @@ def tariffs(file: dict) -> tuple[list, int]:
             values.append(value)
         values = numpy.array([values])
         ans = numpy.concatenate((ans, values), axis=0)
-    return ans.tolist(), ans.shape[0]
+    return ans.tolist(), ans.shape[0], needed_keys
 
 
 def download(file: dict, name: str) -> bool:
@@ -104,5 +108,11 @@ def download(file: dict, name: str) -> bool:
     return True
 
 
-def check_keys():
-    pass
+def check_keys(keys: list) -> list | None:
+    needed_keys = list()
+    if 'nmId' in keys:
+        n = keys.index('nmId')
+        needed_keys.append(n)
+        return needed_keys
+    else:
+        return
--- sheet.py
+++ sheet.py
@@ -10,8 +10,7 @@ class Sheet:
         self.parameters = None
         self.get_parameters()
 
-    @staticmethod
-    def create(service: apiclient.discovery.build, spreadsheetId: str, name_of_sheet: str, dateFrom: str, date: str,
+    def create(self, service: apiclient.discovery.build, spreadsheetId: str, name_of_sheet: str, dateFrom: str, date: str,
                flag: str, limit: str, dateTo: str, from_rk: str, to_rk: str):
         """Создаёт список под названием 'name_of_sheet' с данными из сервера Wildberries"""
         try:
@@ -22,7 +21,7 @@ class Sheet:
             print(f"Нет доступа к файлу '{name_of_sheet}' на сервере")
             return
         try:
-            values, dist = convert_to_list(json_response, name_of_sheet)
+            values, dist, needed_keys = convert_to_list(json_response, name_of_sheet)
         except TypeError:
             print('Файл скачен на устройство')
             return
@@ -48,6 +47,9 @@ class Sheet:
         valueInputOption = "RAW"
         majorDimension = "ROWS"  # список - строка
         distance = f"{name_of_sheet}"
+        with open('sheets.txt', 'r') as txt:
+            sheets = dict(map(lambda x: x.split('='), txt.read().split('\n')))
+            sheetId = sheets[name_of_sheet]
         results = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body={
             "valueInputOption": valueInputOption,
             "data": [
@@ -58,21 +60,24 @@ class Sheet:
             ]
         }).execute()
         print("Updating complete!\n")
+        self.change_formats(service, spreadsheetId, needed_keys=needed_keys, sheetId=sheetId)
         # print(results)
 
-    @staticmethod
-    def update(service: apiclient.discovery.build, spreadsheetId: str, name_of_sheet: str, dateFrom: str, date: str,
+    def update(self, service: apiclient.discovery.build, spreadsheetId: str, name_of_sheet: str, dateFrom: str, date: str,
                flag: str, limit: str, dateTo: str, from_rk: str, to_rk: str):
         """Очищает и обновляет список под названием 'name_of_sheet' с данными из сервера Wildberries"""
         # Данные воспринимаются, как вводимые пользователем (считается значение формул)
-        json_response, status_code = RequestWildberries().start(name_of_sheet=name_of_sheet, dateFrom=dateFrom,
-                                                                date=date, flag=flag,
-                                                                limit=limit, dateTo=dateTo, from_rk=from_rk,
-                                                                to_rk=to_rk)
         try:
-            values, dist = convert_to_list(json_response, name_of_sheet)
-        except Exception as _ex:
-            print(_ex)
+            json_response, status_code = RequestWildberries().start(name_of_sheet=name_of_sheet, dateFrom=dateFrom,
+                                                                    date=date, flag=flag, limit=limit, dateTo=dateTo,
+                                                                    from_rk=from_rk, to_rk=to_rk)
+        except TypeError:
+            print(f"There is no access to the file '{name_of_sheet}' on the server of Wildberries\n")
+            return
+        try:
+            values, dist, needed_keys = convert_to_list(json_response, name_of_sheet)
+        except TypeError:
+            print(f"The file '{name_of_sheet}' has been downloaded to the device")
             return
         distance = f"{name_of_sheet}"
         valueInputOption = "USER_ENTERED"
@@ -82,7 +87,7 @@ class Sheet:
         with open('sheets.txt', 'r') as txt:
             sheets = dict(map(lambda x: x.split('='), txt.read().split('\n')))
             sheetId = sheets[name_of_sheet]
-        print(f"\nStart clearing sheet '{name_of_sheet}' ...")
+        print(f"\nStart clearing sheet '{name_of_sheet}'...")
         results = service.spreadsheets().values().clear(spreadsheetId=spreadsheetId, range=name_of_sheet
                                                         ).execute()
         # results = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={
@@ -107,7 +112,30 @@ class Sheet:
             ]
         }).execute()
         print("Updating complete!\n")
+        self.change_formats(service, spreadsheetId, needed_keys=needed_keys, sheetId=sheetId)
         # print(results)
+
+    @staticmethod
+    def change_formats(service: apiclient.discovery.build, spreadsheetId: str, needed_keys, sheetId):
+        results = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={
+            "requests": [
+                {
+                    "repeatCell": {
+                        "range": {
+                            "sheetId": sheetId,
+                            "startColumnIndex": needed_keys[0],
+                            "endColumnIndex": needed_keys[0] + 1
+                        },
+                        "cell": {
+                            "userEnteredValue": {"stringValue": "1000"},
+                            "formattedValue": "1000",
+                            "userEnteredFormat": {"numberFormat": {"type": "NUMBER", "pattern": "0,00"}}
+                        },
+                        "fields": "userEnteredFormat(numberFormat)"
+                    }
+                }
+            ]
+        }).execute()
 
     def get_parameters(self):
         """Достаёт словарь параметров в parameters.txt"""
