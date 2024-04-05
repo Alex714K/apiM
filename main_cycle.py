772af12d052ff5ecc7e270bfe8a1cb114e0355c1
Александр <klimentenko.ad@gmail.com>
2024-04-06 00:27:33 +0300

    Исправил запрос в цикле

--- main_cycle.py
+++ main_cycle.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     schedule.every(1).day.at("05:05").do(API.start, name_of_sheet='orders_today', dateFrom='today', flag='1')
     schedule.every(1).day.at("05:10").do(API.start, name_of_sheet='tariffs_boxes', date='tariffs')
     schedule.every(1).day.at("05:15").do(API.start, name_of_sheet='tariffs_pallet', date='tariffs')
-    schedule.every().wednesday.at("17:00").do(API.start, name_of_sheet='statements', dateFrom='tariffs')
+    schedule.every().wednesday.at("17:00").do(
+        API.start, name_of_sheet='statements', dateFrom='statements', dateTo='statements'
+    )
     schedule.every(30).minutes.do(API.start, name_of_sheet='prices', limit='1000', filterNmID='')
     print(schedule.get_jobs())
     while True:
