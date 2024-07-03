80701e89e922b04965ebd10fc63ee9ce5fad0055
Александр <klimentenko.ad@gmail.com>
2024-07-03 19:19:45 +0300

    Добавил Ozon и пофиксил некоторые мелкие баги

--- main_cycle.py
+++ main_cycle.py
@@ -1,11 +1,11 @@
-from Api import Api
 import schedule
-import logging
 from cycles.WB_cycle import WB_cycle
+from cycles.Ozon_cycle import Ozon_cycle
 
 
 if __name__ == "__main__":
     WB_cycle()
+    Ozon_cycle()
 
     print(schedule.get_jobs())
     while True:
