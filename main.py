e0a84ef1e6cd13a5761bb5ace5a231021a22de15
Александр <klimentenko.ad@gmail.com>
2024-07-04 15:52:50 +0300

    Поменял, как раньше было (не нужное было изменение)

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from Api import Api
 
 
 if __name__ == "__main__":
-    # Api().start('stocks', 'planeta', 'WB', dateFrom='2024-03-25')
-    # Api().start('orders_1mnth', 'planeta', 'WB', dateFrom='1mnth')
-    # Api().start('orders_1week', 'planeta', 'WB', dateFrom='1week')
-    # Api().start('orders_2days', 'planeta', 'WB', dateFrom='2days')
-    # Api().start('orders_today', 'planeta', 'WB', dateFrom='today', flag='1')
-    # Api().start('tariffs_boxes', 'planeta', 'WB', date='tariffs')
-    # Api().start('tariffs_pallet', 'planeta', 'WB', date='tariffs')
-    # Api().start('prices', 'planeta', 'WB', limit='1000')
-    # Api().start('fixed_prices', 'planeta', 'WB', limit='1000')
-    # Api().start('storage_paid', 'planeta', 'WB', dateFrom='storage_paid') not work
-    # Api().start('rk', 'planeta', 'WB', from_rk='2024-02-01', to_rk='2024-02-29')
-    # Api().start('statements', 'planeta', 'WB', dateFrom='statements')
-
-    # Api().start('analytics', 'grand', 'Ozon')
-    # Api().start('stock_on_warehouses', 'grand', 'Ozon')
-    pass
+    API = Api()
+    # API.start('stocks', 'planeta', 'WB', dateFrom='2024-03-25')
+    # API.start('orders_1mnth', 'planeta', 'WB', dateFrom='1mnth')
+    # API.start('orders_1week', 'planeta', 'WB', dateFrom='1week')
+    # API.start('orders_2days', 'planeta', 'WB', dateFrom='2days')
+    # API.start('orders_today', 'planeta', 'WB', dateFrom='today', flag='1')
+    # API.start('tariffs_boxes', 'planeta', 'WB', date='tariffs')
+    # API.start('tariffs_pallet', 'planeta', 'WB', date='tariffs')
+    # API.start('prices', 'planeta', 'WB', limit='1000')
+    # API.start('fixed_prices', 'planeta', 'WB', limit='1000')
+    # API.start('storage_paid', 'planeta', 'WB', dateFrom='storage_paid') not work
+    # API.start('rk', 'planeta', 'WB', from_rk='2024-02-01', to_rk='2024-02-29')
+    # API.start('statements', 'planeta', 'WB', dateFrom='statements')
+    #
+    # API.start('analytics', 'grand', 'Ozon')
+    # API.start('stock_on_warehouses', 'grand', 'Ozon')
