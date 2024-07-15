3507c7bc08317199866beebfa3e0ad81ec7b6f8e
Александр <klimentenko.ad@gmail.com>
2024-07-15 15:37:02 +0300

    Добавил новое расписание

--- cycles/Ozon_cycle.py
+++ cycles/Ozon_cycle.py
@@ -8,3 +8,7 @@ def Ozon_cycle():
     schedule.every().day.at("01:30").do(API.start, 'analytics', 'grand', 'Ozon')
     schedule.every().day.at("01:35").do(API.start, 'stock_on_warehouses', 'grand', 'Ozon')
     schedule.every().day.at("01:40").do(API.start, 'products', 'grand', 'Ozon')
+    # Вечер гранд
+    schedule.every().day.at("16:00").do(API.start, 'analytics', 'grand', 'Ozon')
+    schedule.every().day.at("16:05").do(API.start, 'stock_on_warehouses', 'grand', 'Ozon')
+    schedule.every().day.at("16:10").do(API.start, 'products', 'grand', 'Ozon')
