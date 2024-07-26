7a5f24724d74d011f25b100686e89a21c89a3d68
Александр <klimentenko.ad@gmail.com>
2024-07-26 19:12:28 +0300

    Моё главное творение - логгер

---
+++ Logger/Logger.py
@@ -0,0 +1,58 @@
+import logging
+import logging.config
+
+
+def getLogger(name_of_logger: str):
+    dictLogConfig = {
+        "version": 1,
+        "handlers": {
+            "fileHandler": {
+                "class": "logging.FileHandler",
+                "formatter": "myFormatter",
+                "filename": "Logger/logs.log",
+                "level": "INFO",
+                "encoding": "UTF-8"
+            },
+            "rootHandler": {
+                "class": "logging.FileHandler",
+                "formatter": "myFormatter",
+                "filename": "Logger/all_trash.log",
+                "level": "DEBUG",
+                "encoding": "UTF-8"
+            },
+            "consoleHandler": {
+                "class": "logging.StreamHandler",
+                "formatter": "myFormatter",
+                "level": "INFO"
+            }
+        },
+        "loggers": {
+            "root": {
+                "handlers": ["rootHandler"],
+                "level": "DEBUG",
+            },
+            "RequestWB": {
+                "handlers": ["fileHandler"],
+                "level": "DEBUG"
+            },
+            "RequestOzon": {
+                "handlers": ["fileHandler"],
+                "level": "DEBUG"
+            },
+            "ApiWB": {
+                "handlers": ["fileHandler"],
+                "level": "DEBUG"
+            },
+            "ApiOzon": {
+                "handlers": ["fileHandler"],
+                "level": "DEBUG"
+            }
+        },
+        "formatters": {
+            "myFormatter": {
+                "format": "%(levelname)s -> %(asctime)s: %(name)s - %(message)s"
+            }
+        }
+    }
+    logging.config.dictConfig(dictLogConfig)
+    return logging.getLogger(name_of_logger)
