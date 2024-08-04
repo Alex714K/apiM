e23a8ae5db78c3c1249401408f707751bf9bbc4d
Маруся <klimentenko.ad@gmail.com>
2024-08-05 01:40:54 +0300

    Убрал аналитику у терехов и днк

--- plugins/cycles/Ozon_cycle.py
+++ plugins/cycles/Ozon_cycle.py
@@ -1,5 +1,6 @@
 import logging
 import schedule
+import time
 from plugins.Api import Api
 
 
@@ -24,28 +25,28 @@ def Ozon_cycle():
     schedule.every().day.at("08:40").do(API.start, 'orders_1week', 'grand', 'Ozon')
     schedule.every().day.at("08:45").do(API.start, 'orders_2days', 'grand', 'Ozon')
     # Утро гранд
-    schedule.every().day.at("02:55").do(API.start, 'analytics', 'terehov', 'Ozon')
+    # schedule.every().day.at("02:55").do(API.start, 'analytics', 'terehov', 'Ozon')
     schedule.every().day.at("03:00").do(API.start, 'stock_on_warehouses', 'terehov', 'Ozon')
     schedule.every().day.at("03:05").do(API.start, 'products', 'terehov', 'Ozon')
     schedule.every().day.at("03:10").do(API.start, 'orders_1mnth', 'terehov', 'Ozon')
     schedule.every().day.at("03:15").do(API.start, 'orders_1week', 'terehov', 'Ozon')
     schedule.every().day.at("03:20").do(API.start, 'orders_2days', 'terehov', 'Ozon')
     # Вечер гранд
-    schedule.every().day.at("08:50").do(API.start, 'analytics', 'terehov', 'Ozon')
+    # schedule.every().day.at("08:50").do(API.start, 'analytics', 'terehov', 'Ozon')
     schedule.every().day.at("08:55").do(API.start, 'stock_on_warehouses', 'terehov', 'Ozon')
     schedule.every().day.at("09:00").do(API.start, 'products', 'terehov', 'Ozon')
     schedule.every().day.at("09:05").do(API.start, 'orders_1mnth', 'terehov', 'Ozon')
     schedule.every().day.at("09:10").do(API.start, 'orders_1week', 'terehov', 'Ozon')
     schedule.every().day.at("09:15").do(API.start, 'orders_2days', 'terehov', 'Ozon')
     # Утро гранд
-    schedule.every().day.at("03:25").do(API.start, 'analytics', 'dnk', 'Ozon')
+    # schedule.every().day.at("03:25").do(API.start, 'analytics', 'dnk', 'Ozon')
     schedule.every().day.at("03:30").do(API.start, 'stock_on_warehouses', 'dnk', 'Ozon')
     schedule.every().day.at("03:35").do(API.start, 'products', 'dnk', 'Ozon')
     schedule.every().day.at("03:40").do(API.start, 'orders_1mnth', 'dnk', 'Ozon')
     schedule.every().day.at("03:45").do(API.start, 'orders_1week', 'dnk', 'Ozon')
     schedule.every().day.at("03:50").do(API.start, 'orders_2days', 'dnk', 'Ozon')
     # Вечер гранд
-    schedule.every().day.at("09:20").do(API.start, 'analytics', 'dnk', 'Ozon')
+    # schedule.every().day.at("09:20").do(API.start, 'analytics', 'dnk', 'Ozon')
     schedule.every().day.at("09:25").do(API.start, 'stock_on_warehouses', 'dnk', 'Ozon')
     schedule.every().day.at("09:30").do(API.start, 'products', 'dnk', 'Ozon')
     schedule.every().day.at("09:35").do(API.start, 'orders_1mnth', 'dnk', 'Ozon')
