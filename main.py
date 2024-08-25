04bdb200d9faf1fff1fed40e43a9c7deffe5e091
Александр <klimentenko.ad@gmail.com>
2024-08-25 16:42:24 +0300

    Добавил ненужный stat_prodvigene

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     # API.start('rk', 'planeta', 'WB')
     # API.start('statements', 'planeta', 'WB')
     # API.start('nm_report', 'grand', 'WB')
+    # API.start('stat_prodvigene', 'planeta', 'WB')
     # Ozon
     # API.start('analytics', 'grand', 'Ozon')
     # API.start('stock_on_warehouses', 'grand', 'Ozon')
